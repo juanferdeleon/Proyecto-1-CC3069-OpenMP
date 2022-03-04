c44d1c44cb5b17f6b257c494bf5f50094fad51e2
juanferdeleon <dele17822@uvg.edu.gt>
2022-03-04 09:27:29 -0600

    Constantes

--- main.cpp
+++ main.cpp
@@ -20,4 +20,15 @@ using namespace std;
 
 int main(){
 
+    //Declaring variable
+	int nx = 11;                        // Número de intervalos discretos
+	double dt = 0.001;                  // intervalo de tiempo
+	double dx = 0.1;                    // intervalo discreto de la distancia
+	double r = dt / dx / dx;            // Constante de discretacion
+	double error = 0;                   // Precisión o diferencia requerida
+
+
+	vector<double> u(nx);               // Vector solución
+	vector<double> u_old(nx);           // Vector solución
+
 }

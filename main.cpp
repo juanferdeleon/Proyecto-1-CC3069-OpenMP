3773a381e0782048e024dd055d6d0f3e90550da8
juanferdeleon <dele17822@uvg.edu.gt>
2022-03-04 09:01:31 -0600

    First commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+/*
+
+        Proyecto #1
+Programación Paralela con OpenMP
+
+    Programa Secuencial
+
+Creado por:
+
+Juan Fernando De Leon Quezada       17822
+Jose Amado Garcia                   18
+Cristopher Barrios                  18
+
+*/
+
+#include<iostream>
+#include<fstream>
+#include<vector>
+using namespace std;
+
+int main(){
+
+}
